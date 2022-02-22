d24174586c34dcb0649b3871f543ab938644e0ab
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2022-02-22 06:24:55 +0300

    Add project files.

---
+++ UdemyIdentityServer.API2/Controllers/PicturesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using UdemyIdentityServer.API2.Models;
+
+namespace UdemyIdentityServer.API2.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class PicturesController : ControllerBase
+    {
+        public IActionResult GetPictures()
+        {
+            var pictures = new List<Picture>()
+            {
+                new Picture(){ Id = 1, Name ="Doğa Resmi",Url="dogaresmi.jpg" },
+                new Picture(){ Id = 1, Name ="Fil Resmi",Url="filresmi.jpg" },
+                new Picture(){ Id = 1, Name ="Aslan Resmi",Url="aslanresmi.jpg" },
+                new Picture(){ Id = 1, Name ="Fare Resmi",Url="fareresmi.jpg" },
+                new Picture(){ Id = 1, Name ="Kedi Resmi",Url="kediresmi.jpg" },
+                new Picture(){ Id = 1, Name ="Köpek Resmi",Url="kopekresmi.jpg" },
+            };
+            return Ok(pictures);
+        }
+    }
+}
